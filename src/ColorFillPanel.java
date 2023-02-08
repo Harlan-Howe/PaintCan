9166aa20115f3a8be8a6f218f150e2c99855520b
harlanhowe <harlan.howe@kinkaid.org>
2023-02-07 23:27:53 -0600

    added paintcomponent. added code to deepcopy start->workingImage added workingImageMutex

--- src/ColorFillPanel.java
+++ src/ColorFillPanel.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.awt.Color;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 import java.io.File;
 
 
@@ -12,11 +14,13 @@ public class ColorFillPanel extends JPanel
     private BufferedImage workingImage;
     private File lastFile = null;
 
+    private Object workingImageMutex;
+
     public ColorFillPanel()
     {
         super();
         activeColor = Color.RED;
-
+        workingImageMutex = new Object();
     }
 
     public Color getActiveColor() {
@@ -26,7 +30,18 @@ public class ColorFillPanel extends JPanel
     public void setActiveColor(Color activeColor) {
         this.activeColor = activeColor;
     }
-
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        synchronized (workingImageMutex)
+        {
+            g.setColor(activeColor);
+            g.fillRect(0,0, getWidth(), 2);
+            if (workingImage != null)
+                g.drawImage(myCanvas, 0, 2, this);
+        }
+    }
     public void doLoadImage()
     {
         JFileChooser chooser = new JFileChooser();
@@ -42,6 +57,32 @@ public class ColorFillPanel extends JPanel
             String fileName = lastFile.getPath();
             startImage = ((BufferedImage)(new ImageIcon(fileName).getImage()));
         }
-        repaint();
+        resetWorkingImage();
+    }
+
+    /**
+     * deepcopies startImage into workingImage, thus resetting the image onscreen back to the original. (synchronized)
+     */
+    public void resetWorkingImage()
+    {
+        synchronized (workingImageMutex)
+        {
+            workingImage = deepcopy(startImage);
+        }
+    }
+
+
+
+    /**
+     * Deep copies the given BufferedImage's data into a new memory bank. Changing one image will not change the other.
+     * @param bufferImage - the image to copy
+     * @return the deep copy of this image.
+     * @author https://bytenota.com/java-cloning-a-bufferedimage-object/
+     */
+    public static BufferedImage deepcopy(BufferedImage bufferImage) {
+        ColorModel colorModel = bufferImage.getColorModel();
+        WritableRaster raster = bufferImage.copyData(null);
+        boolean isAlphaPremultiplied = colorModel.isAlphaPremultiplied();
+        return new BufferedImage(colorModel, raster, isAlphaPremultiplied, null);
     }
 }
